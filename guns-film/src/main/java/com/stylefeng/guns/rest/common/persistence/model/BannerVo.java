3c29aaa2d3a2a3fb70947f6a11ac5146f5b3a5fb
cookiedesert <zfpcumt@outlook.com>
2019-11-27 20:34:21 +0800

    ck

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/model/BannerVo.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Intellij IDEA
+ * User:cookie
+ * Date:2019/11/27
+ * Time:20:30
+ **/
+package com.stylefeng.guns.rest.common.persistence.model;
+
+import lombok.Data;
+
+@Data
+public class BannerVo {
+    private String bannerId;
+    private String bannerAddress;
+    private String bannerUrl;
+}
