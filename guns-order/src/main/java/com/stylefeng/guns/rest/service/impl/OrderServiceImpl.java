91f932697b4cfb4db9ac170592b7c7730242d4a7
Your Name <you@example.com>
2019-12-02 15:18:28 +0800

    12/2

--- guns-order/src/main/java/com/stylefeng/guns/rest/service/impl/OrderServiceImpl.java
+++ guns-order/src/main/java/com/stylefeng/guns/rest/service/impl/OrderServiceImpl.java
@@ -6,15 +6,13 @@ import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.rest.common.persistence.dao.MoocOrderTMapper;
 import com.stylefeng.guns.rest.common.persistence.model.MoocOrderT;
 import com.stylefeng.guns.rest.service.OrderService;
-<<<<<<< HEAD
 import org.springframework.stereotype.Component;
-=======
 import com.stylefeng.guns.rest.vo.BaseResponVO;
 import com.stylefeng.guns.rest.vo.user.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
->>>>>>> 307eefe33bad97c0afbf1cff235444d88425455c
+
 
 @Component
 @Service(interfaceClass = OrderService.class)
