770e1d7dec87fd729798c3419240e2e62f2b64b1
location663 <463818914@qq.com>
2019-12-03 17:21:29 +0800

    puppy

--- guns-promo/src/main/java/com/stylefeng/guns/rest/service/impl/PromoServiceImpl.java
+++ guns-promo/src/main/java/com/stylefeng/guns/rest/service/impl/PromoServiceImpl.java
@@ -2,13 +2,16 @@ package com.stylefeng.guns.rest.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.rest.common.persistence.dao.MtimePromoMapper;
 import com.stylefeng.guns.rest.common.persistence.dao.MtimePromoOrderMapper;
 import com.stylefeng.guns.rest.common.persistence.dao.MtimePromoStockMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MtimePromo;
 import com.stylefeng.guns.rest.service.CinemaService;
 import com.stylefeng.guns.rest.service.PromoService;
 import com.stylefeng.guns.rest.vo.BaseResponVO;
 import com.stylefeng.guns.rest.vo.cinema.CinemaGetConditionVO;
+import com.stylefeng.guns.rest.vo.promo.PromoInfoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -32,7 +35,13 @@ public class PromoServiceImpl implements PromoService {
     public BaseResponVO getPromo(CinemaGetConditionVO cinemaGetConditionVO) {
 
 
+        EntityWrapper<MtimePromo> promoEntityWrapper = new EntityWrapper<>();
+        promoEntityWrapper.eq("cinemaId", );
+        promoMapper.
+
 
         return null;
     }
+
+
 }
