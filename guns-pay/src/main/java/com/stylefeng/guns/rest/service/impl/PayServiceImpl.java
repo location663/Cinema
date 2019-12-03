b019ad1c0d7f24d3375ed17e5a7eedb66987644b
lxmxiao <397115709@qq.com>
2019-12-03 17:20:12 +0800

    lxmxiao

--- guns-pay/src/main/java/com/stylefeng/guns/rest/service/impl/PayServiceImpl.java
+++ guns-pay/src/main/java/com/stylefeng/guns/rest/service/impl/PayServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Service(interfaceClass = PayService.class)
@@ -41,15 +42,28 @@ public class PayServiceImpl implements PayService {
 
 
     @Override
-    public BaseResponVO getPayResult(Integer orderId) throws CinemaParameterException {
-        OrderInfoVO byId = orderService.getById(orderId);
-        BaseResponVO baseResponVO = new BaseResponVO();
-        baseResponVO.setData(byId);
-        if (byId.getOrderStatus().equals("1")){
-            baseResponVO.setStatus(0);
-        } else {
-            baseResponVO.setStatus(1);
+    public BaseResponVO getPayResult(Integer orderId) {
+
+        Integer orderStatus = alipay.test_trade_query(orderId);
+
+        if (orderStatus == 1) {
+            orderService.updateStatusById(orderStatus, orderId);
+            Map<String,Object> map = new HashMap<>();
+            map.put("orderId",orderId);
+            map.put("orderStatus",orderStatus);
+            map.put("orderMsg","支付成功");
+            BaseResponVO baseResponVO = new BaseResponVO(0,null,map,null,null,null);
+            return baseResponVO;
         }
-        return baseResponVO;
+
+//        OrderInfoVO byId = orderService.getById(orderId);
+//        BaseResponVO baseResponVO = new BaseResponVO();
+//        baseResponVO.setData(byId);
+//        if (byId.getOrderStatus().equals("1")){
+//            baseResponVO.setStatus(0);
+//        } else {
+//            baseResponVO.setStatus(1);
+//        }
+        return new BaseResponVO(1,null,null,null,null,null);
     }
 }
